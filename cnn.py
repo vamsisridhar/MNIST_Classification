e709d5bbcd0df17ba98be22c0d432db7a1da8d93
Vamsi Sridhar <vamsisridhar@hotmail.com>
2020-12-29 20:09:29 +0000

    cnn init 1

--- cnn.py
+++ cnn.py
@@ -1,2 +1,8 @@
 import tensorflow as tf
 from tensorflow.keras.layers import Conv2D, ReLU, MaxPooling2D
+from tensorflow.keras import Model
+
+class CNN(Model):
+    def __init__(self):
+        super(CNN, self).__init__()
+
